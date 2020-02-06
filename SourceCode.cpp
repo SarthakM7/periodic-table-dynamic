0c3f5ffd00f78b62ef572384bc6d078a26b03cc3
Sarthak Mishra <sarthak51mishra@gmail.com>
2020-02-07 01:25:13 +0530

    origin

--- SourceCode.cpp
+++ SourceCode.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 void display(string atomicNumber)
 {
-	#changeslolololol#
-	#part2lololol#
 	fstream myFile("element.txt",ios::out | ios::in);
 	int count = 1;
 	cout<<"Atomic Number : "<<atomicNumber<<endl;
